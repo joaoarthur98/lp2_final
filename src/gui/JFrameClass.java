cc07f275362a1d67d3558e97c28d991e527cacdf
João Arthur <joaoarthur@info.ufrn.br>
2021-09-01 22:32:38 -0300

    Esqueleto do menu arquivo

--- src/gui/JFrameClass.java
+++ src/gui/JFrameClass.java
@@ -8,6 +8,9 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class JFrameClass extends JFrame {
 
@@ -35,6 +38,24 @@ public class JFrameClass extends JFrame {
 	public JFrameClass() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu menuArquivo = new JMenu("Arquivo");
+		menuBar.add(menuArquivo);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Abrir");
+		menuArquivo.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Salvar");
+		menuArquivo.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Salvar como");
+		menuArquivo.add(mntmNewMenuItem_2);
+		
+		JMenu mnNewMenu = new JMenu("Editar");
+		menuBar.add(mnNewMenu);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
