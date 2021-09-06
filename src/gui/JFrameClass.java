8790fec4eec6dce79601e9b015cc570186a2f674
João Arthur <joaoarthur@info.ufrn.br>
2021-09-05 22:15:48 -0300

    Botão de Abrir, Salvar e Salvar como

--- src/gui/JFrameClass.java
+++ src/gui/JFrameClass.java
@@ -2,23 +2,42 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.HeadlessException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class JFrameClass extends JFrame {
 
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel contentPane = new JPanel();;
+	private JMenuBar menuBar = new JMenuBar();
+	private JMenu menuArquivo = new JMenu("Arquivo");
+	private JMenuItem abrir = new JMenuItem("Abrir");
+	private JMenuItem salvar = new JMenuItem("Salvar");
+	private JMenuItem salvarComo = new JMenuItem("Salvar como");
+	private JMenu mnNewMenu = new JMenu("Editar");
+	private JTextArea textArea = new JTextArea();
+	private JFileChooser fileChooser = new JFileChooser();
+	private final JLabel caminhoArquivo = new JLabel("");
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -32,40 +51,102 @@ public class JFrameClass extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public JFrameClass() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 700, 500);
 		
-		JMenuBar menuBar = new JMenuBar();
+		this.menuBar = new JMenuBar();
+		this.menuArquivo = new JMenu("Arquivo");
+		
 		setJMenuBar(menuBar);
 		
-		JMenu menuArquivo = new JMenu("Arquivo");
 		menuBar.add(menuArquivo);
 		
-		JMenuItem mntmNewMenuItem = new JMenuItem("Abrir");
-		menuArquivo.add(mntmNewMenuItem);
+		abrir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					browseFiles(e);
+				} catch (IOException exception) {
+					exception.printStackTrace();
+				}
+			}
+		});
 		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Salvar");
-		menuArquivo.add(mntmNewMenuItem_1);
+		salvar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				escrever(fileChooser.getSelectedFile());
+			}
+		});
 		
-		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Salvar como");
-		menuArquivo.add(mntmNewMenuItem_2);
+		salvarComo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				escrever(fileChooser.getSelectedFile());
+			}
+		});
 		
-		JMenu mnNewMenu = new JMenu("Editar");
+		menuArquivo.add(abrir);
+		menuArquivo.add(salvar);
+		menuArquivo.add(salvarComo);
+		
 		menuBar.add(mnNewMenu);
-		contentPane = new JPanel();
+		
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
+		
 		setContentPane(contentPane);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		contentPane.add(scrollPane, BorderLayout.CENTER);
 		
-		JTextArea textArea = new JTextArea();
 		scrollPane.setViewportView(textArea);
+	}
+	
+	public void browseFiles(ActionEvent e) throws IOException {
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
+		fileChooser.setFileFilter(filter);
+		
+		fileChooser.showOpenDialog(this);
+		fileChooser.setVisible(true);
+		contentPane.add(caminhoArquivo, BorderLayout.SOUTH);
+		caminhoArquivo.setText(fileChooser.getSelectedFile().getAbsolutePath());
+		leitor(fileChooser.getSelectedFile());
+	}
+	
+	public void leitor(File file) throws IOException {
+		BufferedReader buffRead = new BufferedReader(new FileReader(file.getAbsolutePath()));
+		String linha = "";
+		while (true) {
+			if (linha != null) {
+				textArea.append(linha + "\n");
+			} else
+				break;
+			linha = buffRead.readLine();
+		}
+		buffRead.close();
+	}
+	
+	public void escrever(File file) {
+		try {
+			
+			if(file == null) {
+				salvarComo();
+			} else {
+				FileWriter fileWriter = new FileWriter(file.getAbsolutePath());
+				fileWriter.write(textArea.getText());
+				fileWriter.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void salvarComo() {
+		int returnVal = fileChooser.showSaveDialog(this);
+		
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+			File file = fileChooser.getSelectedFile();
+			escrever(file);
+		}
 	}
 
 }
