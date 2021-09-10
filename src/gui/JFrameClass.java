ad76c76659b6ed37e8cf6209aa8ba4e899d7aef6
João Arthur <joaoarthur@info.ufrn.br>
2021-09-10 20:51:39 -0300

    Adição de abas e número da linha atual de edição

--- src/gui/JFrameClass.java
+++ src/gui/JFrameClass.java
@@ -2,7 +2,6 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.HeadlessException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -16,8 +15,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.swing.text.BadLocationException;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
@@ -26,21 +25,24 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 
-public class JFrameClass extends JFrame implements CaretListener{
+public class JFrameClass extends JFrame implements CaretListener {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private JPanel contentPane = new JPanel();;
 	private JMenuBar menuBar = new JMenuBar();
 	private JMenu menuArquivo = new JMenu("Arquivo");
+	private JMenuItem novo = new JMenuItem("Novo");
 	private JMenuItem abrir = new JMenuItem("Abrir");
 	private JMenuItem salvar = new JMenuItem("Salvar");
 	private JMenuItem salvarComo = new JMenuItem("Salvar como");
 	private JMenu mnNewMenu = new JMenu("Editar");
-	private JTextArea textArea = new JTextArea();
+	private JTextArea textAreaGlobal = new JTextArea();
 	private JFileChooser fileChooser = new JFileChooser();
 	private final JLabel caminhoArquivo = new JLabel("");
-	
+	private JScrollPane scrollPane = new JScrollPane();
+	private JTabbedPane abas = new JTabbedPane();
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -57,14 +59,27 @@ public class JFrameClass extends JFrame implements CaretListener{
 	public JFrameClass() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 500);
-		
+
 		this.menuBar = new JMenuBar();
 		this.menuArquivo = new JMenu("Arquivo");
-		
+
 		setJMenuBar(menuBar);
-		
+
 		menuBar.add(menuArquivo);
-		
+
+		novo.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					novo(fileChooser.getSelectedFile());
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+
+		});
+
 		abrir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -74,72 +89,96 @@ public class JFrameClass extends JFrame implements CaretListener{
 				}
 			}
 		});
-		
+
 		salvar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				escrever(fileChooser.getSelectedFile());
 			}
 		});
-		
+
 		salvarComo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				escrever(fileChooser.getSelectedFile());
 			}
 		});
-		
+
+		menuArquivo.add(novo);
 		menuArquivo.add(abrir);
 		menuArquivo.add(salvar);
 		menuArquivo.add(salvarComo);
-		
+
 		menuBar.add(mnNewMenu);
-		
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		
-		setContentPane(contentPane);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		contentPane.add(scrollPane, BorderLayout.CENTER);
-		
-		scrollPane.setViewportView(textArea);
-		
-		
-		contentPane.add(caminhoArquivo, BorderLayout.SOUTH);
-		
+	}
+
+	public void browseFiles(ActionEvent e) throws IOException {
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
+		fileChooser.setFileFilter(filter);
+
+		fileChooser.showOpenDialog(this);
+		fileChooser.setVisible(true);
+
+		JPanel panel = new JPanel();
+
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		panel.setLayout(new BorderLayout(0, 0));
+
+		setContentPane(panel);
+
+		panel.add(scrollPane, BorderLayout.CENTER);
+
+		panel.add(caminhoArquivo, BorderLayout.SOUTH);
+
+		add(BorderLayout.CENTER, abas);
+
+		JTextArea textArea = leitor2(fileChooser.getSelectedFile());
+
+		JScrollPane scrollPane2 = new JScrollPane();
+		scrollPane2.setViewportView(textArea);
+
+		abas.addTab(fileChooser.getSelectedFile().getName(), scrollPane2);
+
+		atualizarLinhasAtuais(textArea);
+
+		caminhoArquivo.setText("Caminho: " + fileChooser.getSelectedFile().getAbsolutePath());
+	}
+
+	private void atualizarLinhasAtuais(JTextArea textArea) {
 		textArea.addCaretListener(new CaretListener() {
 			@Override
 			public void caretUpdate(CaretEvent e) {
 				int lineNum = 1;
-				
+
 				try {
-					int pos = textArea.getCaretPosition();
-					lineNum = textArea.getLineOfOffset(pos);
-					
+					int pos = textAreaGlobal.getCaretPosition();
+					lineNum = textAreaGlobal.getLineOfOffset(pos);
+
 					lineNum += 1;
 				} catch (Exception e1) {
 					e1.printStackTrace();
 				}
-				
+
 				updateStatus(lineNum);
 			}
 		});
-		
 	}
-	
-	public void browseFiles(ActionEvent e) throws IOException {
-		FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
-		fileChooser.setFileFilter(filter);
-		
-		fileChooser.showOpenDialog(this);
-		fileChooser.setVisible(true);
-		
-		caminhoArquivo.setText("Caminho: " + fileChooser.getSelectedFile().getAbsolutePath());
-		leitor(fileChooser.getSelectedFile());
-	}
-	
+
 	public void leitor(File file) throws IOException {
 		BufferedReader buffRead = new BufferedReader(new FileReader(file.getAbsolutePath()));
 		String linha = "";
+		while (true) {
+			if (linha != null) {
+				textAreaGlobal.append(linha + "\n");
+			} else
+				break;
+			linha = buffRead.readLine();
+		}
+		buffRead.close();
+	}
+
+	public JTextArea leitor2(File file) throws IOException {
+		BufferedReader buffRead = new BufferedReader(new FileReader(file.getAbsolutePath()));
+		String linha = "";
+		JTextArea textArea = new JTextArea();
 		while (true) {
 			if (linha != null) {
 				textArea.append(linha + "\n");
@@ -148,29 +187,44 @@ public class JFrameClass extends JFrame implements CaretListener{
 			linha = buffRead.readLine();
 		}
 		buffRead.close();
+		textAreaGlobal = textArea;
+		return textArea;
 	}
-	
+
 	public void escrever(File file) {
 		try {
-			
-			if(file == null) {
+			if (file == null) {
 				salvarComo();
 			} else {
 				FileWriter fileWriter = new FileWriter(file.getAbsolutePath());
-				fileWriter.write(textArea.getText());
+				fileWriter.write(textAreaGlobal.getText());
 				fileWriter.close();
 			}
-			
+
 			caminhoArquivo.setText("Caminho: " + fileChooser.getSelectedFile().getAbsolutePath());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public void escreverNovo() {
+		fileChooser.showSaveDialog(this);
+		
+		File file = fileChooser.getSelectedFile();
+		FileWriter fileWriter;
+		try {
+			fileWriter = new FileWriter(file.getAbsolutePath());
+			fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		caminhoArquivo.setText("Caminho: " + fileChooser.getSelectedFile().getAbsolutePath());
+	}
+
 	public void salvarComo() {
 		int returnVal = fileChooser.showSaveDialog(this);
-		
-		if(returnVal == JFileChooser.APPROVE_OPTION) {
+
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
 			File file = fileChooser.getSelectedFile();
 			escrever(file);
 		}
@@ -181,10 +235,35 @@ public class JFrameClass extends JFrame implements CaretListener{
 			caminhoArquivo.setText("Line: " + linenumber);
 		else
 			caminhoArquivo.setText("Caminho: " + fileChooser.getSelectedFile().getAbsolutePath() + "\t Line: " + linenumber);
-    }
+	}
 
 	@Override
 	public void caretUpdate(CaretEvent e) {
+	}
+
+	private void novo(File file) throws IOException {
+		escreverNovo();
+		JPanel panel = new JPanel();
+
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		panel.setLayout(new BorderLayout(0, 0));
+
+		setContentPane(panel);
+
+		panel.add(scrollPane, BorderLayout.CENTER);
+
+		panel.add(caminhoArquivo, BorderLayout.SOUTH);
+
+		add(BorderLayout.CENTER, abas);
+
+		JTextArea textArea = leitor2(fileChooser.getSelectedFile());
+
+		JScrollPane scrollPane2 = new JScrollPane();
+		scrollPane2.setViewportView(textArea);
+
+		abas.addTab(fileChooser.getSelectedFile().getName(), scrollPane2);
+
+		atualizarLinhasAtuais(textArea);
 	}
 
 }
